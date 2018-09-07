d8100d64148f61216934cd85bab9854edd96778c
Adam Duff <j.adam.duff@gmail.com>
2018-09-07 07:22:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if is_logged_in?(session)
+    if Helpers.is_logged_in?(session)
       erb :account
     else
       erb :error
