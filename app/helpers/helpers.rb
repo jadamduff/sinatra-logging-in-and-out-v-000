b4915bfc360dd9972f091ba0e5ac7df19e669d03
Adam Duff <j.adam.duff@gmail.com>
2018-09-07 07:08:09 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
-  def current_user(session)
+  def self.current_user(session)
     user = User.find(session[:id])
   end
 
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
     session[:id] != nil ? true : false
   end
   
